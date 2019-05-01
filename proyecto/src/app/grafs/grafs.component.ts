439ea6abbe068602198c75c976bfa7007a8433cf
luishdezh <43009665+luishdezh@users.noreply.github.com>
2019-05-01 04:48:48 -0500

    grafs errores

---
+++ proyecto/src/app/grafs/grafs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grafs',
+  templateUrl: './grafs.component.html',
+  styleUrls: ['./grafs.component.css']
+})
+export class GrafsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
