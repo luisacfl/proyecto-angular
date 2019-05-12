5b72fd791cf9cbdb0565da869e70e0cc7c36b790
luisacfl <luisa.fl.97@gmail.com>
2019-05-12 09:18:15 -0500

    connection to mongodb

--- proyecto/server.js
+++ proyecto/server.js
@@ -1,33 +1,38 @@
 'use strict'
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const dbConfig = require('./config/database.js');
-const mongoose = require('mongoose');
+let {Desaparecidx} = require('./mongodb/mongodb-connect');
 
 const app = express();
 const port = 3000;
 
+
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+let jsonParser = bodyParser.json();
+let corsOptions = {
+    origin: 'http://127.0.0.1:5500',
+    optionsSuccessStatus: 200
+}
 
-mongoose.Promise = global.Promise;
+app.use(jsonParser);
+app.use(cors(corsOptions));
 
-// Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
-
-app.get('/', (req, res) => res.json({"mensaje":"Página funciona"}));
-app.route('/home').get((req, res)=>res.json({"mensaje":"Página funciona"}));
+app.use(express.static(__dirname + '/public'));
 
 // routes ======================================================================
-const r = require('./src/app/routes.js');
+app.route('/api/desap')
+    .get((req, res) => {
+        Desaparecidx.find({},{_id:0,prim_nombre:1},(err,docs)=>{
+            if(err){
+                res.status(404).send();
+                return;
+            }
+            res.json(docs);
+        })
+    });
+
 
 // listen (start app with node server.js) ======================================
 app.listen(port, ()=>console.log(`Example app listening on port ${port}! `));
